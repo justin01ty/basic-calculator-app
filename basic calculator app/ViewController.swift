1bc98793890e693aef5b377a90ba78e89ad761db
Justin Gist <justin.gist@smail.astate.edu>
2019-09-01 20:23:57 -0500

    Initial Commit

---
+++ basic calculator app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  basic calculator app
+//
+//  Created by Justin Gist on 9/1/19.
+//  Copyright © 2019 Jus Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
